34939908a50113494f8d08a610ef8159e78237e0
doldollee00 <50581232+doldollee00@users.noreply.github.com>
2026-01-21 13:42:49 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 68.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        int i = -1;
+        i = my_string.indexOf(target);
+        
+        if(i>=0){
+            return answer = 1;
+        }
+        return answer;
+    }
+}
