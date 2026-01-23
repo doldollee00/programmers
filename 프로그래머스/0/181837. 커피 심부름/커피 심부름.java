af3a97cdbb2945a9f102c18dec83de9fc5f15ac7
doldollee00 <50581232+doldollee00@users.noreply.github.com>
2026-01-23 10:29:47 +0900

    [level 0] Title: 커피 심부름, Time: 0.65 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         for(int i=0; i<order.length; i++) {
-            if(order[i].indexOf("latte") >= 0) {
+            if(order[i].contains("latte")) {
                 answer += 5000;
             } else {
                 answer += 4500;
